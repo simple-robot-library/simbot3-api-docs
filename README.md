c826d970c81f8bd22c75b838fe02da5fe00c2651
ForteScarlet <ForteScarlet@users.noreply.github.com>
2023-02-08 18:02:12 +0000

    deploy: 660fdf31e06532309dfcc601b38bc33a75a36841

--- README.md
+++
@@ -1,5 +0,0 @@
-# simbot3-api-docs
-与simbot3相关的API文档部署仓库站点: [docs.simbot.forte.love](https://docs.simbot.forte.love)
-
-用于记录与引导核心库以及组件库的KDoc和仓库的相关链接。
-
--- components/tencent-guild/scripts/sourceset_dependencies.js
+++
@@ -1,1 +0,0 @@
-sourceset_dependencies = '{":simbot-component-tencent-guild-stdlib:dokkaHtmlPartial/main":[],":simbot-component-tencent-guild-core:dokkaHtmlPartial/main":[],":simbot-component-tencent-guild-boot:dokkaHtmlPartial/main":[],":simbot-component-tencent-guild-api:dokkaHtmlPartial/main":[]}'
