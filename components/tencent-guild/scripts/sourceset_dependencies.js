9fddabc71398c6fb0d6911ab225164ba7c746c83
ForteScarlet <ForteScarlet@users.noreply.github.com>
2023-02-08 18:36:20 +0000

    deploy: simple-robot/simbot-component-tencent-guild@b3b054129675a05010670c99bef24f630dbc4ddd

---
+++ components/tencent-guild/scripts/sourceset_dependencies.js
@@ -0,0 +1,1 @@
+sourceset_dependencies = '{":simbot-component-tencent-guild-stdlib:dokkaHtmlPartial/main":[],":simbot-component-tencent-guild-core:dokkaHtmlPartial/main":[],":simbot-component-tencent-guild-boot:dokkaHtmlPartial/main":[],":simbot-component-tencent-guild-api:dokkaHtmlPartial/main":[]}'
