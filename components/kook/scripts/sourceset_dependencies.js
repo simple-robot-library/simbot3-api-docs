2df7d7500263dbf0f7073caf2eed36d246851c1d
ForteScarlet <ForteScarlet@users.noreply.github.com>
2023-02-08 18:40:58 +0000

    deploy: e614023a486d841d64683b1f7586f756299da8b9

--- components/kook/scripts/sourceset_dependencies.js
+++
@@ -1,1 +0,0 @@
-sourceset_dependencies = '{":simbot-component-kook-core:dokkaHtmlPartial/main":[],":simbot-component-kook-stdlib:dokkaHtmlPartial/main":[],":simbot-component-kook-api:dokkaHtmlPartial/main":[]}'
