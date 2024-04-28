0267e4a504a988e4e5c4062dea6fe01e451c3998
ForteScarlet <ForteScarlet@163.com>
2024-04-28 20:22:42 +0800

    grid

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -9,59 +9,64 @@
         <n-layout position="absolute" style="top: 150px; bottom: 64px" :native-scrollbar="false"
                   :content-style="layoutCenterContentStyle">
 
-          <n-flex vertical>
-            <HorizontalAdUnit />
-            <n-collapse :default-expanded-names="[...collapseActions]" @item-header-click="handleItemHeaderClick">
-              <n-card embedded :bordered="false">
-                <n-collapse-item name="KDoc">
-                  <RepoDocCards/>
-                  <template #header>
-                    <n-divider title-placement="left" style="user-select: none">
-                      <Icon>
-                        <BookOutline/>
-                      </Icon> &nbsp;
-                      <n-text strong style="font-size: 19px">KDoc</n-text>
-                    </n-divider>
-                    <n-divider title-placement="center">
-                      <n-collapse-transition :show="!collapseActions.has('KDoc')">
-                        <n-text italic depth="3" style="user-select: none; font-size: 6px">点击展开</n-text>
-                      </n-collapse-transition>
-                    </n-divider>
-                    <n-divider title-placement="right">
-                      <n-collapse-transition :show="collapseActions.has('KDoc')">
-                        <n-text italic depth="3" style="user-select: none; font-size: 13px">Javadoc的好朋友</n-text>
-                      </n-collapse-transition>
-                    </n-divider>
-                  </template>
-                </n-collapse-item>
-              </n-card>
-              <n-card embedded :bordered="false">
-                <n-collapse-item name="作品展示">
-                  <ShowCards/>
-                  <template #header>
-                    <n-divider title-placement="left" style="user-select: none">
-                      <n-text strong style="font-size: 19px">作品展示</n-text>
-                      &nbsp;
-                      <Icon>
-                        <HeartOutline/>
-                      </Icon>
-                    </n-divider>
-                    <n-divider title-placement="center">
-                      <n-collapse-transition :show="!collapseActions.has('作品展示')">
-                        <n-text italic depth="3" style="user-select: none; font-size: 6px">点击展开</n-text>
-                      </n-collapse-transition>
-                    </n-divider>
-                    <n-divider title-placement="right">
-                      <n-collapse-transition :show="collapseActions.has('作品展示')">
-                        <n-text italic depth="3" style="user-select: none; font-size: 13px">看看大家的好宝贝</n-text>
-                      </n-collapse-transition>
-                    </n-divider>
-                  </template>
-                </n-collapse-item>
-              </n-card>
-            </n-collapse>
-          </n-flex>
-
+          <n-grid cols="8" item-responsive responsive="screen">
+            <n-grid-item span="0 m:1 l:1 xl:2"></n-grid-item>
+            <n-grid-item span="8 m:6 l:6 xl:4">
+              <n-flex vertical>
+                <HorizontalAdUnit />
+                <n-collapse :default-expanded-names="[...collapseActions]" @item-header-click="handleItemHeaderClick">
+                  <n-card embedded :bordered="false">
+                    <n-collapse-item name="KDoc">
+                      <RepoDocCards/>
+                      <template #header>
+                        <n-divider title-placement="left" style="user-select: none">
+                          <Icon>
+                            <BookOutline/>
+                          </Icon> &nbsp;
+                          <n-text strong style="font-size: 19px">KDoc</n-text>
+                        </n-divider>
+                        <n-divider title-placement="center">
+                          <n-collapse-transition :show="!collapseActions.has('KDoc')">
+                            <n-text italic depth="3" style="user-select: none; font-size: 6px">点击展开</n-text>
+                          </n-collapse-transition>
+                        </n-divider>
+                        <n-divider title-placement="right">
+                          <n-collapse-transition :show="collapseActions.has('KDoc')">
+                            <n-text italic depth="3" style="user-select: none; font-size: 13px">Javadoc的好朋友</n-text>
+                          </n-collapse-transition>
+                        </n-divider>
+                      </template>
+                    </n-collapse-item>
+                  </n-card>
+                  <n-card embedded :bordered="false">
+                    <n-collapse-item name="作品展示">
+                      <ShowCards/>
+                      <template #header>
+                        <n-divider title-placement="left" style="user-select: none">
+                          <n-text strong style="font-size: 19px">作品展示</n-text>
+                          &nbsp;
+                          <Icon>
+                            <HeartOutline/>
+                          </Icon>
+                        </n-divider>
+                        <n-divider title-placement="center">
+                          <n-collapse-transition :show="!collapseActions.has('作品展示')">
+                            <n-text italic depth="3" style="user-select: none; font-size: 6px">点击展开</n-text>
+                          </n-collapse-transition>
+                        </n-divider>
+                        <n-divider title-placement="right">
+                          <n-collapse-transition :show="collapseActions.has('作品展示')">
+                            <n-text italic depth="3" style="user-select: none; font-size: 13px">看看大家的好宝贝</n-text>
+                          </n-collapse-transition>
+                        </n-divider>
+                      </template>
+                    </n-collapse-item>
+                  </n-card>
+                </n-collapse>
+              </n-flex>
+            </n-grid-item>
+            <n-grid-item span="0 m:1 l:1 xl:2"></n-grid-item>
+          </n-grid>
 
         </n-layout>
         <n-layout-footer
@@ -88,6 +93,8 @@ import {
   NConfigProvider,
   NDivider,
   NFlex,
+  NGrid,
+  NGridItem,
   NLayout,
   NLayoutFooter,
   NLayoutHeader,
@@ -103,10 +110,10 @@ import ShowCards from "./show/ShowCards.vue";
 import HorizontalAdUnit from "./ad/HorizontalAdUnit.vue";
 
 const layoutCenterContentStyle = reactive({
-    'padding-left': '50px',
-    'padding-right': '50px',
-    'padding-top': '50px',
-    'padding-bottom': '50px',
+    // 'padding-left': '50px',
+    // 'padding-right': '50px',
+    // 'padding-top': '50px',
+    // 'padding-bottom': '50px',
 })
 
 const initThemeActiveValue = localStorage.getItem("theme_dark") != null
